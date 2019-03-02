bfbbe7853ad51b674f642c3e99890095c9f88206
Rob Juurlink <rob.juurlink@rabobank.nl>
2019-03-02 16:38:43 +0100

    debug and verbose parameters are optional

--- src/thuisbezorgd-scraper.js
+++ src/thuisbezorgd-scraper.js
@@ -11,8 +11,8 @@ exports.scrape = scrape;
  * @param {Object} configuration Configuration object with the properties 'username', 'password', 'debug' (optional) and 'verbose' (optional)
  * @param {string} configuration.username Username
  * @param {string} configuration.password Password
- * @param {boolean} configuration.debug If true, show debugging info
- * @param {boolean} configuration.verbose If true, show verbose logging
+ * @param {boolean} [configuration.debug] If true, show debugging info
+ * @param {boolean} [configuration.verbose] If true, show verbose logging
  * @return {Promise} A promise that resolves with all the orders as JSON object
  */
 function scrape(configuration) {
